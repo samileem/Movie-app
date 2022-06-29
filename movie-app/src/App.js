552a4a21d352badcb34d12ee8427d232ed384716
samileem <76707695+samileem@users.noreply.github.com>
2022-06-29 15:36:52 -0500

    Fixed issue with map returning null

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,83 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import MovieList from './components/MovieList';
-import MovieListHeading from './components/MovieListHeading';
-import SearchBox from './components/SearchBox';
-import AddFavorite from './components/AddtoFavorites';
-import RemoveFavorites from './components/RemoveFavorites';
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavorite from "./components/AddtoFavorites";
+import RemoveFavorites from "./components/RemoveFavorites";
 
 const App = () => {
-	const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
   const [favorites, setFavorites] = useState([]);
-  
+
   const getMovieRequest = async (searchValue) => {
-        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
-        const response = await fetch(url);
-        const responseJson = await response.json();
+    const response = await fetch(url);
+    const responseJson = await response.json();
 
-        if (responseJson.Search) {
-          setMovies(responseJson.Search);
-        }
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
   };
 
-            
   useEffect(() => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
   useEffect(() => {
-		const movieFavorites = JSON.parse(
-			localStorage.getItem('react-movie-app-favorites')
-		);
+    const movieFavorites = JSON.parse(
+      localStorage.getItem("react-movie-app-favorites")
+    ) ?? [];
 
-  setFavorites(movieFavorites);
-    }, []);	
+    setFavorites(movieFavorites);
+  }, []);
 
-    const saveToLocalStorage = (items) => {
-      localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
-    };
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favorites", JSON.stringify(items));
+  };
 
-    const addFavoriteMovie = (movie) => {
-      const newFavoriteList = [...favorites, movie];
-      setFavorites(newFavoriteList);
-      saveToLocalStorage(newFavoriteList);
-            };
+  const addFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
 
-    const removeFavoriteMovie = (movie) => {
-      const newFavoriteList = favorites.filter(
-        (favorite) =>favorite.imdbID !== movie.imdbID
-      );
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorite) => favorite.imdbID !== movie.imdbID
+    );
 
     setFavorites(newFavoriteList);
     saveToLocalStorage(newFavoriteList);
-    };
+  };
 
-	return (
-		<div className='container-fluid movie-app'>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Movies" />
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-        </div>
-      <div className='row'>
-        <MovieList 
-        movies={movies} 
-        favoriteComponent={AddFavorite}
-        handleFavoritesClick={addFavoriteMovie} />
       </div>
-      <div className='row d-flex align-items-center mt-4 mb-4'>
-        <MovieListHeading heading='Favorites'/>
+      <div className="row">
+        <MovieList
+          movies={movies}
+          favoriteComponent={AddFavorite}
+          handleFavoritesClick={addFavoriteMovie}
+        />
       </div>
-      <div className='row'>
-        <MovieList 
-        movies={favorites} 
-        handleFavoritesClick={removeFavoriteMovie}
-        favoriteComponent={RemoveFavorites}/>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favorites" />
       </div>
-		</div>
-	);
+      <div className="row">
+        <MovieList
+          movies={favorites}
+          handleFavoritesClick={removeFavoriteMovie}
+          favoriteComponent={RemoveFavorites}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- movie-app/src/components/MovieList.js
+++ movie-app/src/components/MovieList.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const MovieList = (props) => {
     const FavoriteComponent = props.favoriteComponent;
-
+	
 	return (
 		<>
-			{props.movies.map((movie, index) => (
+			{props.movies?.map((movie) => (
 				<div className='image-container d-flex justify-content-start m-3'>
 					<img src={movie.Poster} alt='movie'></img>
                     <div 
